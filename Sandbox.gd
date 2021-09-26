320ec57ea199e9ccf6d953af981503cd4dbb5a37
Johnny Stene <jhonnystene@protonmail.com>
2021-09-26 11:57:17 -0600

    More 1.1.0 stuff

--- Sandbox.gd
+++ Sandbox.gd
@@ -18,6 +18,8 @@ const TYPE_SALT = {"color": Color("#FFFFFF"), "density": 3, "flammable": false, 
 # Liquid
 const TYPE_WATER = {"color": Color(0, 112, 217), "density": 1, "flammable": false, "physics_passes": 3, "physics_material": "liquid", "physics_chance": 75, "simulation_material": "water", "viscosity": 70, "flow_direction": -1}
 const TYPE_OIL = {"color": Color("#6e0b46"), "density": 2, "flammable": true, "burn_time": 0.3, "physics_passes": 2, "physics_material": "liquid", "physics_chance": 75, "simulation_material": "none", "viscosity": 95, "flow_direction": -1}
+const TYPE_ACID = {"color": Color("#066b17"), "density": 1, "flammable": false, "physics_passes": 3, "physics_material": "liquid", "physics_chance": 75, "simulation_material": "acid", "viscosity": 70, "flow_direction": -1}
+const TYPE_LAVA = {"color": Color("#e07800"), "density": 3, "flammable": false, "physics_passes": 2, "physics_material": "liquid", "physics_chance": 75, "simulation_material": "lava", "viscosity": 70, "flow_direction": -1}
 
 # Gas
 const TYPE_STEAM = {"color": Color("#9fd6fc"), "density": 0, "flammable": false, "physics_passes": 1, "physics_material": "rising", "physics_chance": 75, "flow_direction": -1, "simulation_material": "steam"}
@@ -25,13 +27,14 @@ const TYPE_METHANE = {"color": Color("#7a08a3"), "density": 0, "flammable": true
 
 # Solid
 const TYPE_WOOD = {"color": Color("#634f1a"), "density": 5, "flammable": true, "burn_time": 3, "physics_material": "solid", "simulation_material": "none"}
+const TYPE_STONE = {"color": Color("#8c8c8c"), "density": 5, "flammable": false, "burn_time": 3, "physics_material": "solid", "simulation_material": "none"}
 const TYPE_FUSE = {"color": Color("#27a308"), "density": 5, "flammable": true, "burn_time": 0.3, "physics_material": "solid", "simulation_material": "none"}
 
 # Burning
 const TYPE_FIRE = {"color": Color("#edab26"), "density": 9, "flammable": false, "physics_material": "none", "simulation_material": "fire", "time_to_live": 0.1}
 const TYPE_EMBER = {"color": Color("#d8eb10"), "density": 0, "flammable": false, "physics_passes": 1, "physics_material": "rising", "physics_chance": 75, "flow_direction": -1, "simulation_material": "fire", "time_to_live": 1}
 
-const TYPE_TRANSLATION = {"Sand": TYPE_SAND, "Water": TYPE_WATER, "Oil": TYPE_OIL, "Fire": TYPE_FIRE, "Wood": TYPE_WOOD, "Fuse": TYPE_FUSE, "Methane": TYPE_METHANE, "Salt": TYPE_SALT}
+const TYPE_TRANSLATION = {"Sand": TYPE_SAND, "Water": TYPE_WATER, "Oil": TYPE_OIL, "Fire": TYPE_FIRE, "Wood": TYPE_WOOD, "Fuse": TYPE_FUSE, "Methane": TYPE_METHANE, "Salt": TYPE_SALT, "Acid": TYPE_ACID, "Stone": TYPE_STONE, "Lava": TYPE_LAVA}
 
 var selected = TYPE_SAND
 
@@ -172,7 +175,7 @@ func ProcessWorld(delta):
 			
 			# MATERIAL SIMULATION
 			if(IsMaterial(pixel, "fire")):
-				var spread_rate = 25
+				var spread_rate = 15
 				if(IsFlammable(x - 1, y - 1) and rand_range(0, 100) < spread_rate):
 					SetPixel(x - 1, y - 1, TYPE_FIRE, {"time_to_live": world[x - 1][y - 1]["burn_time"]})
 				if(IsFlammable(x, y - 1) and rand_range(0, 100) < spread_rate):
@@ -212,6 +215,21 @@ func ProcessWorld(delta):
 			elif(IsMaterial(pixel, "salt")):
 				if(IsInWater(x, y) and rand_range(0, 100) < 15):
 					SetPixel(x, y, TYPE_WATER)
+			elif(IsMaterial(pixel, "acid")):
+				if not(IsSwappable(x, y, x, y + 1)):
+					if(rand_range(0, 100) < 25):
+						SetPixel(x, y + 1, TYPE_AIR)
+			elif(IsMaterial(pixel, "lava")):
+				if(y != worldHeight - 1):
+					if not(IsSwappable(x, y, x, y + 1)):
+						if(rand_range(0, 100) < 25 && (IsFlammable(x, y + 1) or IsMaterial(world[x][y + 1], "fire"))):
+							SetPixel(x, y + 1, TYPE_AIR)
+						
+				if(x != 0 and IsFlammable(x - 1, y)):
+					SetPixel(x - 1, y, TYPE_FIRE)
+				
+				if(x != worldWidth - 1 and IsFlammable(x + 1, y)):
+					SetPixel(x + 1, y, TYPE_FIRE)
 			
 			# PHYSICS SIMULATION
 			if(IsType(pixel, TYPE_AIR)): # Air
@@ -287,6 +305,8 @@ func UpdateSandboxImage():
 	$Display.texture.set_flags(0)
 			
 func _process(delta):
+	if($UI/Paused.pressed):
+		return
 	selected = TYPE_TRANSLATION[$UI/OptionButton.text]
 	
 	if(Input.is_mouse_button_pressed(BUTTON_LEFT)):
